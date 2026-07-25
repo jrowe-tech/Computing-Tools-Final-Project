2a046d97c06213663d14b6f28169152d247897a7
jrowe-tech <jroweschool@gmail.com>
2026-07-24 22:05:48 -0400

    Update Terraform Plan

--- terraform/main.tf
+++ terraform/main.tf
@@ -8,3 +8,17 @@ terraform {
 }
 
 provider "docker" {}
+
+resource "docker_image" "app" {
+  name = "app_image:latest"
+}
+
+resource "docker_container" "app" {
+  name  = "app_container"
+  image = docker_image.app.image_id
+
+  ports {
+    internal = 5000
+    external = 5000
+  }
+}
